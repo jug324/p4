c4c1e8ed1d53ee88495f885742d2c956039ee98f
Justin Gilman <justingilman@g.harvard.edu>
2014-11-22 08:35:23 -0500

    Created routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,38 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/login', function()
+{
+	return 'This is the login page';
+});
+
+Route::get('/student', function()
+{
+	return 'This is the page for students/characters';
+});
+
+Route::get('/regions', function()
+{
+	return 'This is the regions page';
+});
+
+Route::get('/orders', function()
+{
+	return 'This is the Title Orders page';
+});
+
+Route::get('/specialties', function()
+{
+	return 'This is the specialty description page';
+});
+
+Route::get('/races', function()
+{
+	return 'This is the race description page';
+});
+
+Route::get('/school', function()
+{
+	return 'This page lists all students created';
+});
