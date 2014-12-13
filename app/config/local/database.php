7506293e4fb291c47b1716e4232ac2902c6a1161
Justin Gilman <justingilman@g.harvard.edu>
2014-12-13 09:01:56 -0500

    Created environment file

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -25,7 +25,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'hewytt',
 			'username'  => 'root',
-			'password'  => 'cykygUcp53',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
