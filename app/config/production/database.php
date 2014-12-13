8d2efcf8c03c2bb4c07a000bdd693606689d2c80
Justin Gilman <justingilman@g.harvard.edu>
2014-12-13 08:08:53 -0500

    Created production database file

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'hewytt',
+            'username'  => 'root',
+            'password'  => 'cykygUcp53',
+        ),
+
+    ),
+
+);
